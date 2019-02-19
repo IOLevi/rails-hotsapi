cff291caa5c91bbfa4d78643775bef908337123f
IOLevi <337@holbertonschool.com>
2019-02-19 09:20:45 +0000

    change uglifier to harmony true

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,6 @@ module RailsHotsapi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     # config.assets.initialize_on_precompile = false
-    config.assets.js_compressor = Uglifier.new(harmony: true)
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
