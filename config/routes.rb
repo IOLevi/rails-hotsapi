b5a4a5a6993c73069bb25aee41d18a69aa7d5c81
IOLevi <337@holbertonschool.com>
2019-02-19 05:14:10 +0000

    rewrite navbar with logic for session

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
   root 'static_pages#home'
   get '/bans', to: 'static_pages#bans'
   get '/signup', to: 'users#new'
@@ -8,6 +10,9 @@ Rails.application.routes.draw do
   get '/api/v1/supports', to: 'api#supports'
   get '/api/v1/assassins', to: 'api#assassins'
   post '/signup', to: 'users#create'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   resources :users
   
   # root
